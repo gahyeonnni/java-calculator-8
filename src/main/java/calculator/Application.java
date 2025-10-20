feddb490235df7d068dbd87ff07f1ab9a54c388e
김가현 <hyeonnni@naver.com>
2025-10-20 19:04:34 +0900

    feat: 계산기 실행 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.calculator.CalculateService;
+import calculator.calculator.Calculator;
+import calculator.numstring.NumStringService;
+import calculator.view.ViewService;
+
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args){
+        CalculateService calculateService = new CalculateService();
+        NumStringService numStringService = new NumStringService();
+        ViewService viewService = new ViewService();
+
+        Calculator calculator = new Calculator(calculateService, numStringService, viewService);
+        calculator.run();
     }
 }
